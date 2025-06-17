b1464e719e84dd7e4f5397b9c4e69ca66d1c2653
yhharmonia <yhong@harmonia.com>
2025-06-17 08:46:20 -0400

    Added Dockerfile

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL =  'https://czkkuw93y9.us-east-1.awsapprunner.com/api';
+//'http://localhost:8000/api';
 
 export interface ApiIssue {
   id: number;
